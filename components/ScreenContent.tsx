7401709d7b3d98dd3c2b908b1ee92b740ca69d66
Andrew Wang <andrew.wang@polarfi.com>
2025-02-12 14:48:18 -0800

    Fix tab

--- components/ScreenContent.tsx
+++ components/ScreenContent.tsx
@@ -14,76 +14,76 @@ const StyledLine = cssInterop(Line, {
 
 export const ScreenContent = () => {
   const rawColorPrimaryHsl = useUnstableNativeVariable('--color-primary');
-    const colorPrimaryHsl = `hsl(${rawColorPrimaryHsl?.split(' ').join(', ')})`;
-    return (
-      <View className="flex-1 gap-3">
-        <View className="mx-4 h-72">
-          <Text className="text-red-500">
-            Setting className=&quot;text-red-500&quot;
-          </Text>
-          <CartesianChart
-            data={data}
-            xKey="date"
-            yKeys={['balance']}
-            xAxis={{ lineWidth: 0 }}
-            yAxis={[{ lineWidth: 0 }]}
-          >
-            {({ points }) => (
-              <>
-                <StyledLine
-                  points={points.balance}
-                  strokeWidth={1}
-                  className="text-red-500"
-                />
-              </>
-            )}
-          </CartesianChart>
-        </View>
-        <View className="mx-4 h-72">
-          <Text className="text-primary">
-            (Bug) - Setting className=&quot;text-primary&quot;
-          </Text>
-          <CartesianChart
-            data={data}
-            xKey="date"
-            yKeys={['balance']}
-            xAxis={{ lineWidth: 0 }}
-            yAxis={[{ lineWidth: 0 }]}
-          >
-            {({ points }) => (
-              <>
-                <StyledLine
-                  points={points.balance}
-                  strokeWidth={1}
-                  className="text-primary"
-                />
-              </>
-            )}
-          </CartesianChart>
-        </View>
-        <View className="mx-4 h-72">
-          <Text className="text-primary">
-            Setting color=&quot;{colorPrimaryHsl}&quot; from
-            useUnstableNativeVariable(&apos;--color-primary&apos;)
-          </Text>
-          <CartesianChart
-            data={data}
-            xKey="date"
-            yKeys={['balance']}
-            xAxis={{ lineWidth: 0 }}
-            yAxis={[{ lineWidth: 0 }]}
-          >
-            {({ points }) => (
-              <>
-                <StyledLine
-                  points={points.balance}
-                  strokeWidth={1}
-                  color={colorPrimaryHsl}
-                />
-              </>
-            )}
-          </CartesianChart>
-        </View>
+  const colorPrimaryHsl = `hsl(${rawColorPrimaryHsl?.split(' ').join(', ')})`;
+  return (
+    <View className="flex-1 gap-3">
+      <View className="mx-4 h-72">
+        <Text className="text-red-500">
+          Setting className=&quot;text-red-500&quot;
+        </Text>
+        <CartesianChart
+          data={data}
+          xKey="date"
+          yKeys={['balance']}
+          xAxis={{ lineWidth: 0 }}
+          yAxis={[{ lineWidth: 0 }]}
+        >
+          {({ points }) => (
+            <>
+              <StyledLine
+                points={points.balance}
+                strokeWidth={1}
+                className="text-red-500"
+              />
+            </>
+          )}
+        </CartesianChart>
       </View>
-    );
+      <View className="mx-4 h-72">
+        <Text className="text-primary">
+          (Bug) - Setting className=&quot;text-primary&quot;
+        </Text>
+        <CartesianChart
+          data={data}
+          xKey="date"
+          yKeys={['balance']}
+          xAxis={{ lineWidth: 0 }}
+          yAxis={[{ lineWidth: 0 }]}
+        >
+          {({ points }) => (
+            <>
+              <StyledLine
+                points={points.balance}
+                strokeWidth={1}
+                className="text-primary"
+              />
+            </>
+          )}
+        </CartesianChart>
+      </View>
+      <View className="mx-4 h-72">
+        <Text className="text-primary">
+          Setting color=&quot;{colorPrimaryHsl}&quot; from
+          useUnstableNativeVariable(&apos;--color-primary&apos;)
+        </Text>
+        <CartesianChart
+          data={data}
+          xKey="date"
+          yKeys={['balance']}
+          xAxis={{ lineWidth: 0 }}
+          yAxis={[{ lineWidth: 0 }]}
+        >
+          {({ points }) => (
+            <>
+              <StyledLine
+                points={points.balance}
+                strokeWidth={1}
+                color={colorPrimaryHsl}
+              />
+            </>
+          )}
+        </CartesianChart>
+      </View>
+    </View>
+  );
 }
